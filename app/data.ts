001d3324f2fd28cf0b01c9ab24845fd0331846e1
NoobMahbub <Mahbubbengali@gmail.com>
2024-11-25 01:43:53 +0600

    first grid done

---
+++ app/data.ts
@@ -0,0 +1,11 @@
+export const data = [
+    { title: "Alpha", height: 150, value: 75 },
+    { title: "Beta", height: 165, value: 85 },
+    { title: "Gamma", height: 120, value: 50 },
+    { title: "Delta", height: 180, value: 95 },
+    { title: "Epsilon", height: 140, value: 60 },
+    { title: "Zeta", height: 175, value: 88 },
+    { title: "Eta", height: 160, value: 80 },
+    
+  ];
+  
---
+++ components/CssGrid.tsx
@@ -0,0 +1,19 @@
+import { data } from '@/app/data'
+import React from 'react'
+
+const CssGrid = () => {
+    const boxStyle = 'bg-neutral-100 border rounded-md p-4 flex justify-center items-center flex-col'
+  return (
+    <div className='grid sm:grid-cols-3 gap-3 p-4 auto-rows-[300px]'>
+        {data.map((item,i)=>(
+            <div className={`${boxStyle} ${(i===3 || i == 6) &&'sm:col-span-2'}`} 
+            key={i}>
+                <h2>{item.title}</h2>
+                <p className='font-bold'>{item.value}</p>
+            </div>
+        ))}
+    </div>
+  )
+}
+
+export default CssGrid
