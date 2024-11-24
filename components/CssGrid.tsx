a7c4072637f176066bb8ae5c81a59de0ddc74379
NoobMahbub <Mahbubbengali@gmail.com>
2024-11-25 01:57:46 +0600

    second grid done

--- components/CssGrid.tsx
+++ components/CssGrid.tsx
@@ -4,9 +4,9 @@ import React from 'react'
 const CssGrid = () => {
     const boxStyle = 'bg-neutral-100 border rounded-md p-4 flex justify-center items-center flex-col'
   return (
-    <div className='grid sm:grid-cols-3 gap-3 p-4 auto-rows-[300px]'>
+    <div className='grid sm:grid-cols-4 gap-3 p-4 auto-rows-[300px]'>
         {data.map((item,i)=>(
-            <div className={`${boxStyle} ${(i===3 || i == 6) &&'sm:col-span-2'}`} 
+            <div className={`${boxStyle} ${(i===0 || i === 6 || i === 4 || i === 5 || i === 6) && 'sm:col-span-2'} ${(i===2)&& 'sm:row-span-2'}`} 
             key={i}>
                 <h2>{item.title}</h2>
                 <p className='font-bold'>{item.value}</p>
